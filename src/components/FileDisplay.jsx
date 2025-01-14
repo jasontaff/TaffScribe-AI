368704c3cf0a47e9e767620689b802f348569a4d
jtaff <jtaff@2u.com>
2025-01-14 17:41:02 -0500

    start of transcibing with AI

--- src/components/FileDisplay.jsx
+++ src/components/FileDisplay.jsx
@@ -6,9 +6,9 @@ export default function FileDisplay(props) {
 
     
   return (
-    <main className='flex-1 p-4 flex flex-col gap-3 sm:gap-4  justify-center text-center pb-20 w-fit max-w-full mx-auto'>
+    <main className='flex-1 p-4 flex flex-col gap-3 sm:gap-4  justify-center text-center pb-20 w-72 sm:w-96 w-fit max-w-full mx-auto'>
           <h1 className='font-semibold text-4xl sm:text-5xl md:text-6xl'>Your <span className='text-blue-400 bold'>File</span></h1>
-          <div className="mx-auto flex flex-col text-left my-4">
+          <div className="flex flex-col text-left my-4">
             <h3 className='font-semibold'>Name</h3>
             <p>{file ? file.name : 'Custom Audio'}</p>
           </div>
