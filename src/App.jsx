1f46937450234b746221421e0b53de2fdf5eb2f6
jtaff <jtaff@2u.com>
2025-01-23 17:10:04 -0500

    add new components

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import Transcribing from './components/Transcribing'
 function App() {
   const [file, setFile] = useState(null) //file upload
   const [audioStream, setAudioStream] = useState(null) //live recording
-  const [output, setOutput] = useState(null)
+  const [output, setOutput] = useState(true)
   const [loading, setLoading] = useState(true)
 
 
